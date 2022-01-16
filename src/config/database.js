1793925fe204705281df20ac1ca9b91ca1321f3c
gustavo <gustavo@brainyit.com.br>
2022-01-16 16:42:13 -0300

    create database

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,8 @@
 module.exports = {
     dialect: 'postgres',
-    host: 'localhost',
-    username: 'usuario',
-    password: 'password',
+    host: '127.0.0.1',
+    username: 'docker',
+    password: 'P@ssw0rd',
     database: 'datauser-microservices',
     define: {
         timestamps: true,
